df052d3781f61864a3c9ff82fc87e63ad37bc28d
vikramcse <vikramcse.10@gmail.com>
2016-01-02 21:58:07 +0530

    Added Initial setup mocha test

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var mocha = require('mocha');
+var expect = require('chai').expect;
+
+describe('test suite', function () {
+    it('should test', function () {
+        expect(true).to.be.true;
+    });
+});
