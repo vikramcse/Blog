dc2b9be95516dfc8d38e01020e53542bea31b931
vikramcse <vikramcse.10@gmail.com>
2016-01-04 23:02:18 +0530

    Some folder structure changes

---
+++ app/model/database.js
@@ -0,0 +1,18 @@
+var mysql = require('mysql2');
+var connection;
+
+exports.init = function(config) {
+    connection = mysql.createConnection(config);
+};
+
+exports.query = function(sql, params) {
+    return new Promise(function(resolve, reject) {
+        connection.query(sql, params, function(error, result) {
+            if(error) {
+                return reject(error);
+            }
+            resolve(result);
+
+        });
+    });
+};
