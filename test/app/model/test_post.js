ce078ecfa6ba64a8dfca20ef3a5c532fb29efb5b
vikramcse <vikramcse.10@gmail.com>
2016-01-07 22:50:10 +0530

    Added tests for removin posts

--- test/app/model/test_post.js
+++ test/app/model/test_post.js
@@ -7,20 +7,38 @@ var chaiAsPromised = require('chai-as-promised');
 var expect = chai.expect;
 chai.use(chaiAsPromised);
 
-database.init(config.database);
-
-beforeEach(function() {
-    return posts.dropTable().then(function() {
-    	return posts.createTable();
-    });
-});
-
 describe('post model', function() {
-    it('should add a post', function() {
-        return expect(posts.add({
-            title: 'title',
-            body: 'body',
-            slug: 'test'
-        })).to.eventually.equal(1);
+    before(function() {
+        database.init(config.database);
+        return posts.createTable();
     });
+
+    beforeEach(function() {
+        return posts.deleteAll();
+    });
+
+    describe('#add', function() {
+        it('it should add a post', function() {
+            return expect(posts.add({
+                title: 'title',
+                body: 'body',
+                slug: 'test'
+            })).to.eventually.equal(1);
+        });
+    });
+
+    describe('#remove', function() {
+        it('it should delete a post', function() {
+            return expect(
+                posts.add({
+                    title: 'title',
+                    body: 'body',
+                    slug: 'test'
+                }).then(function(id) {
+                    return posts.remove(id);
+                })
+            ).to.eventually.equal(1);
+        });
+    });
+    
 });
